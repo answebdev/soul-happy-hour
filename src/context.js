710fa3834670a510f01fedf37546e1c7e1aaabce
ANS <36783010+answebdev@users.noreply.github.com>
2021-02-07 11:27:13 -0800

    add single cocktail page

--- src/context.js
+++ src/context.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { useCallback } from 'react';
 
+// Search cocktail by name:
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const AppContext = React.createContext();
 
--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,9 +1,129 @@
 import React from 'react';
 import Loading from '../components/Loading';
+
+// useParams is the hook that React Router provides so we can access unique values.
 import { useParams, Link } from 'react-router-dom';
 
+// Lookup full cocktail details by id:
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
 const SingleCocktail = () => {
-  return <div>Single Cocktail Page</div>;
+  const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json();
+        // console.log(data);
+        if (data.drinks) {
+          // Destructure the object (first item in the array)
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0];
+          // Set up ingredients array
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          };
+          setCocktail(newCocktail);
+        } else {
+          setCocktail(null);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
+    getCocktail();
+  }, [id]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (!cocktail) {
+    return <h2 className='section-title'>no cocktail to display</h2>;
+  }
+  // Destructure properties from object,
+  // then place them inside the return
+  const {
+    name,
+    image,
+    category,
+    info,
+    glass,
+    instructions,
+    ingredients,
+  } = cocktail;
+
+  return (
+    <section className='section cocktail-section'>
+      <Link to='/' className='btn btn-primary'>
+        back home
+      </Link>
+      <h2 className='section-title'>{name}</h2>
+      <div className='drink'>
+        <img src={image} alt={name} />
+        <div className='drink-info'>
+          <p>
+            <span className='drink-data'>name:</span>
+            {name}
+          </p>
+          <p>
+            <span className='drink-data'>category:</span>
+            {category}
+          </p>
+          <p>
+            <span className='drink-data'>info:</span>
+            {info}
+          </p>
+          <p>
+            <span className='drink-data'>glass:</span>
+            {glass}
+          </p>
+          <p>
+            <span className='drink-data'>instructions:</span>
+            {instructions}
+          </p>
+          <p>
+            <span className='drink-data'>ingredients:</span>
+            {ingredients.map((item, index) => {
+              // Not every drink in the endpoint has the same number of ingredients, so some will be null.
+              // If there is no ingredient at that particular index in the array, return null.
+              return item ? <span key={index}>{item}</span> : null;
+            })}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SingleCocktail;
