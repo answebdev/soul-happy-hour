4caeed640f2eddaa357a6bb0460a66fc71a7514e
ANS <36783010+answebdev@users.noreply.github.com>
2021-02-07 10:52:43 -0800

    add search form logic

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,12 +5,30 @@ const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
   const searchValue = React.useRef('');
 
+  // Set focus on the input
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className='section search'>
-      <form className='search-form'>
+      <form className='search-form' onSubmit={handleSubmit}>
         <div className='form-control'>
           <label htmlFor='name'>search your favorite cocktail</label>
-          <input type='text' id='name' ref={searchValue} />
+          <input
+            type='text'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
         </div>
       </form>
     </section>
